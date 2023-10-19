f4a6ed81d17daddc5ebc830d2419c2532f0a3583
Joan Steven Henao Bermeo <henaobermeosteven@gmail.com>
2023-10-19 11:18:28 -0500

    Product Presenter

---
+++ Presenters/ProductPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp1.Views;
+using Supermarket_mvp1.Models;
+
+namespace Supermarket_mvp1.Presenters
+{
+    internal class ProductPresenter
+    {
+    }
+}
